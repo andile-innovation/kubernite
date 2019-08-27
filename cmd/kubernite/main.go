dd3d96fd4572b6e90006cadce4fd6f5420a7abc9
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-27 13:41:04 +0200

    get latest git tag or commit hash

--- cmd/kubernite/main.go
+++ cmd/kubernite/main.go
@@ -2,15 +2,58 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/src-d/go-git.v4"
+	"io"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	kubeRestclient "k8s.io/client-go/rest"
+	"log"
 	"os"
 )
 
+var repoPath = "/Users/bernardbussy/go/src/github.com/andile-innovation/james"
+
 func main() {
+
 	var config = new(kubeRestclient.Config)
 	var err error
+
+	r, err := git.PlainOpen(repoPath)
+	if err != nil {
+		log.Fatal("error opening git repo: " + err.Error())
+	}
+
+	deploymentTag := ""
+
+	tagReferences, err := r.Tags()
+	if err != nil {
+		log.Fatal("error getting repo tag refs: " + err.Error())
+	}
+
+	if tagReferences != nil {
+		latestTag, err := tagReferences.Next()
+		switch err {
+		case io.EOF:
+		case nil:
+			deploymentTag = latestTag.Name().Short()
+		default:
+			log.Fatal("error getting latest tag ref: " + err.Error())
+		}
+	}
+
+	if deploymentTag == "" {
+		commitReferences, err := r.CommitObjects()
+		if err != nil {
+			log.Fatal("error getting repo commits: " + err.Error())
+		}
+		commit, err := commitReferences.Next()
+		if err != nil {
+			log.Fatal("error getting latest commit: " + err.Error())
+		}
+		deploymentTag = commit.Hash.String()
+	}
+
+	fmt.Println("use: " + deploymentTag)
 
 	config.Host = os.Getenv("PLUGIN_KUBERNETES_SERVER")
 	config.TLSClientConfig.CAData = []byte(os.Getenv("PLUGIN_KUBERNETES_CERT_DATA"))
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module kubernite
 go 1.12
 
 require (
+	gopkg.in/src-d/go-git.v4 v4.13.1
 	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
 	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
 )
