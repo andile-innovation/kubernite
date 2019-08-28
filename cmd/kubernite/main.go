9ffa1f3d7a0a3389d2a38b0780f8b1b248fd289e
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-28 11:26:45 +0200

    remove dummy variable

--- cmd/kubernite/main.go
+++ cmd/kubernite/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"gopkg.in/src-d/go-billy.v4"
-	"gopkg.in/src-d/go-billy.v4/osfs"
 	"gopkg.in/src-d/go-git.v4"
 	"io"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -12,10 +10,7 @@ import (
 	kubeRestclient "k8s.io/client-go/rest"
 	"log"
 	"os"
-	"path/filepath"
 )
-
-var repoPath = "/Users/bernardbussy/go/src/github.com/andile-innovation/james"
 
 func main() {
 
@@ -70,14 +65,8 @@ func getDeploymentTag() (string, error) {
 		return "", errors.New(fmt.Sprintf("'%s' is not a directory", repositoryPath))
 	}
 
-	r, err := openRepository()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Println(r)
-
 	// try and open repository at given path
-	repository, err := git.PlainOpen(repoPath)
+	repository, err := git.PlainOpen(repositoryPath)
 	if err != nil {
 		return "", errors.New("unable to open repository: " + err.Error())
 	}
@@ -109,46 +98,4 @@ func getDeploymentTag() (string, error) {
 		return "", errors.New("error getting latest repository commit: " + err.Error())
 	}
 	return commit.Hash.String(), nil
-}
-
-func openRepository() (*git.Repository, error) {
-	// confirm that given repository path is valid
-	repositoryPath := os.Getenv("PLUGIN_DEPLOYMENT_TAG_REPOSITORY_PATH")
-	if repositoryPath == "" {
-		return nil, errors.New("deployment tag repository path is blank")
-	}
-	absoluteRepositoryPath, err := filepath.Abs(repositoryPath)
-	if err != nil {
-		return nil, errors.New("error getting absolute repository path: " + err.Error())
-	}
-	f, err := os.Stat(absoluteRepositoryPath)
-	if err != nil {
-		return nil, errors.New("unable to validate given deployment tag repository path: " + err.Error())
-	}
-	if !f.IsDir() {
-		return nil, errors.New(fmt.Sprintf("'%s' is not a directory", absoluteRepositoryPath))
-	}
-
-	// get a billy filesystem for repository .git file
-	var repositoryFS, dotGitFS billy.Filesystem
-	var dotGitFSInfo os.FileInfo
-	repositoryFS = osfs.New(absoluteRepositoryPath)
-	dotGitFSInfo, err = repositoryFS.Stat(".git")
-	if err != nil {
-		return nil, errors.New("unable to get .git file/directory info: " + err.Error())
-	}
-	if !dotGitFSInfo.IsDir() {
-		return nil, errors.New("<deployment_tag_repository_path>/.git is not a directory")
-	}
-
-	dotGitFS, err = repositoryFS.Chroot(".git")
-	if err != nil {
-		return nil, errors.New("unable to get .git directory filesystem: " + err.Error())
-	}
-
-	if _, err := dotGitFS.Stat(""); err != nil {
-		return nil, errors.New("unable to get .git directory filesystem info: " + err.Error())
-	}
-
-	return nil, nil
 }
