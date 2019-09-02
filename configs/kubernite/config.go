1937b87156e0b746b80c07b1e761be61b3cfa9a1
Lawrence <lawrence@andilesolutions.com>
2019-09-02 15:41:21 +0200

    stufff

--- configs/kubernite/config.go
+++ configs/kubernite/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 func init() {
-	err := viper.BindEnv("DeploymentRepositoryPath", "PLUGIN_DEPLOYMENT_REPOSITORY_PATH")
+	err := viper.BindEnv("DeploymentTagRepositoryPath", "PLUGIN_DEPLOYMENT_TAG_REPOSITORY_PATH")
 	err = viper.BindEnv("KubernetesDeploymentFilePath", "PLUGIN_KUBERNETES_DEPLOYMENT_FILE_PATH")
 	err = viper.BindEnv("KubernetesServer", "PLUGIN_KUBERNETES_SERVER")
 	err = viper.BindEnv("KubernetesCertData", "PLUGIN_KUBERNETES_CERT_DATA")
@@ -16,6 +16,10 @@ func init() {
 	err = viper.BindEnv("KubernetesClientKeyData", "PLUGIN_KUBERNETES_CLIENT_KEY_DATA")
 	err = viper.BindEnv("BuildEvent", "DRONE_BUILD_EVENT")
 	err = viper.BindEnv("DryRun", "PLUGIN_DRY_RUN")
+	//deployment_file_path
+	//deployment_image_name
+	//deployment_file_repository_path
+	//commit_deployment
 	if err != nil {
 		err = ErrPackageInitialisation{Reasons: []string{
 			"binding viper keys to environment variables",
